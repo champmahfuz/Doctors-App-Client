3fe14e1911bfaecc656dda6eaf880b0dac9d0681
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2024-02-05 21:48:17 +0600

    setup frontend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-dark">
+        <h2 className="text-success">Hello Hello React</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
